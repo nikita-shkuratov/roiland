4979a61f2ffb605479950015d0eeb7682e796149
skuratoff <ya-skuratoff@yandex.by>
2020-08-31 15:00:28 +0300

    create pagination

--- src/components/Episodes/Episodes.jsx
+++ src/components/Episodes/Episodes.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
@@ -6,20 +6,28 @@ import { fethEpisodes } from '../../redux/actions/actionsList'
 import { PATH_EPISODE } from '../../constRotePath'
 import DataList from '../DataList/DataList'
 import Loader from '../Loader/Loader'
+import Pagination from '../Pagination/Pagination'
 
 function Episodes() {
     const dispatch = useDispatch()
     const episodesList = useSelector(state => state.list.fethedEpisodes)
+    const [currentPage, setCurrentPage] = useState(1)
 
     useEffect(() => {
-        dispatch(fethEpisodes())
-    }, [dispatch])
+        dispatch(fethEpisodes(`?page=${currentPage}`))
+    }, [dispatch,currentPage])
 
     const { results } = episodesList
+    const { info } = episodesList
 
     return (
         <section className='content'>
             <div className='content__block'>
+            <Pagination
+                    currentPage={currentPage}
+                    setCurrentPage={setCurrentPage}
+                    info={info} />
+
                 {!results
                     ? <Loader />
                     : <ul>{results.map(item =>
---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,42 @@
+
+import React from "react"
+import { createPagination } from './createPagination'
+
+function Pagination({ currentPage, setCurrentPage, info }) {
+    const { count } = info ? info : 200
+
+    const { pagination } = createPagination({
+        numberOfArticles: count || 200,
+        articlesPerPage: 20,
+        numberOfButtons: 8,
+        currentPage
+    })
+
+    const handleClick = page => setCurrentPage(page)
+
+    return (
+        <div className="pagination">
+            <ul>
+                <li
+                    className={`${pagination[0] === currentPage && "disabled"}`}
+                    onClick={handleClick.bind(null, currentPage - 1)}>
+                    Prev
+                </li>
+                {pagination.map((page, index) => (
+                    <li
+                        key={index}
+                        className={`${currentPage === page && "active"}`}
+                        onClick={handleClick.bind(null, page)}>
+                        {page}
+                    </li>))}
+                <li
+                    className={`${pagination.reverse()[0] === currentPage && "disabled"}`}
+                    onClick={handleClick.bind(null, currentPage + 1)}>
+                    Next
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Pagination
